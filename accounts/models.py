f4721ec86f9e59a78699c24ed75e51d7dcc625c9
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2024-01-27 04:10:42 +0100

    Added functionality relating to custom users

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # add additional fields in here
+    age = models.PositiveIntegerField(null=True, blank=True)
