d847e63836dcfe84219f9c3f19427c8d090a9231
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2024-01-28 16:03:21 +0100

    Added comments to articles

--- articles/models.py
+++ articles/models.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
+    title = models.CharField(max_length=255) #type: ignore
+    body = models.TextField() #type: ignore
+    date = models.DateTimeField(auto_now_add=True) #type: ignore
+    author = models.ForeignKey( #type: ignore
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )  # new
 
@@ -16,3 +16,17 @@ class Article(models.Model):
 
     def get_absolute_url(self):  # new
         return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments") #type: ignore
+    comment = models.CharField(max_length=140) #type: ignore
+    author = models.ForeignKey( #type: ignore
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse("article_list")
