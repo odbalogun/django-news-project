333cd96231c06f34caa3b8fe33b591973b1bbc3e
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2024-01-28 14:19:48 +0100

    Added permissions to prevent unauthorized updates & deletes

--- articles/views.py
+++ articles/views.py
@@ -1,26 +1,40 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
+from django.urls import reverse_lazy
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body',)
 
-class ArticleListView(ListView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = "article_edit.html"
 
-class ArticleDeleteView(DeleteView):
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): #type: ignore
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy('article_list')
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
